82b6d4c8937a1891a3c09dbb9cfe2a143bcb82bc
Jazlin Yu <jazlinsub@gmail.com>
2023-09-27 14:25:57 -0700

    zombie data added

--- lib/models.py
+++ lib/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+
 class Person(Base):
     __tablename__ = 'people'
 
@@ -12,13 +13,13 @@ class Person(Base):
     health = Column(Integer())
     location_id = Column(Integer(), ForeignKey('locations.id'))
 
-
     location = relationship('Location', back_populates='people')
+
     def __repr__(self):
         return f'Person {self.id}:'\
-        + f'{self.name}'\
-        + f'{self.health}'\
-        + f'{self.location_id}'
+            + f'{self.name}'\
+            + f'{self.health}'\
+            + f'{self.location_id}'
 
 
 class Zombie(Base):
@@ -33,9 +34,10 @@ class Zombie(Base):
 
     def __repr__(self):
         return f'Location {self.id}:'\
-        + f'{self.name}'\
-        + f'{self.zombie.name}'\
-        + f'{self.person.name}'
+            + f'{self.name}'\
+            + f'{self.zombie.name}'\
+            + f'{self.person.name}'
+
 
 class Location(Base):
     __tablename__ = "locations"
@@ -47,4 +49,12 @@ class Location(Base):
     zombies = relationship('Zombie', back_populates='location')
 
 
+class Weapon(Base):
+    __tablename__ = "weapons"
 
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    damage = Column(Integer())
+    
+    people = relationship('Person', back_populates='weapon')
+    zombies = relationship('Zombie', back_populates='weapon')
--- lib/seed.py
+++ lib/seed.py
@@ -3,15 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 
-
-
 if __name__ == "__main__":
 
     engine = create_engine('sqlite:///zombie.db')
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
     session = Session()
-    
 
     house = Location(name="house")
     graveyard = Location(name="graveyard")
@@ -20,23 +17,26 @@ if __name__ == "__main__":
 
     session.add_all([house, graveyard, rv_camp, grocery_store])
 
-
     jane = Person(name="Jane Doe", health=10, location=house)
     john = Person(name="Johnny Simmons", health=7, location=house)
 
     session.add_all([jane, john])
 
-    big_zombie = Zombie(description="Big and brute", health=40, location=graveyard)
-    small_zombie = Zombie(description="Small", health=8, location=grocery_store)
-    medium_zombie =Zombie(description="medium", health=19, location=rv_camp)
+    big_zombie = Zombie(description="Big and brute",
+                        health=40, location=graveyard)
+    small_zombie = Zombie(description="Small", health=8,
+                          location=grocery_store)
+    medium_zombie = Zombie(description="medium", health=19, location=rv_camp)
 
     session.add_all([big_zombie, small_zombie, medium_zombie])
+
+    hand_gun = Weapon(damage=10)
+    rifle = Weapon(damage=20)
+    shotgun = Weapon(damage=40)
+
+    session.add_all([hand_gun, rifle, shotgun])
 
     session.commit()
     session.close()
 
     print("Database updated")
-
-
-
-
